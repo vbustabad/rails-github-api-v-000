d4d7bced7050614a49830cd9353559ceecc942ad
Veronica Bustabad <vbustabad@gmail.com>
2018-10-02 21:22:47 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,13 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+     req.params['oauth_token'] = session[:token]
+     req.params['v'] = '20160201'
+    end
+    @repositories = JSON.parse(resp.body)["response"]["repositories"]["items"]
   end
 
+  # def create
+  # end
 end
