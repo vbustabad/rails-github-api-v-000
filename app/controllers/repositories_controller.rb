e339b089f831b6101da925c1e51b1201aac31db1
Veronica Bustabad <vbustabad@gmail.com>
2018-10-02 23:21:54 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,13 +6,13 @@ class RepositoriesController < ApplicationController
       request.headers['Authorization'] = "token #{session[:token]}"
     end
     binding.pry
-    @login = JSON.parse(response.body)["owner"]["login"]
+    @login = JSON.parse(response.body)["login"]
 
     resp = Faraday.get("https://api.github.com/user/repos") do |req|
       req.body = { 'oauth_token': session[:token] }
       req.headers['Authorization'] = "token #{session[:token]}"
     end
-    @repositories = JSON.parse(resp.body)["owner"]["repo_url"]
+    @repositories = JSON.parse(resp.body)["repo_url"]
   end
 
   # def create
